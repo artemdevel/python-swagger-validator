b7ea35ae0faeef40a1197ce18a5806b00f503028
artemdevel <artem.devel2014@gmail.com>
2015-12-30 19:45:08 +0200

    Changed run_app.py to work with env vars for app address and port

--- run_app.py
+++ run_app.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
+import os
 from wsgiref.simple_server import make_server
+
+from structlog import get_logger
 
 from app import create_app
 
+log = get_logger()
+
 
 if __name__ == '__main__':
-    make_server('127.0.0.1', 8000, create_app()).serve_forever()
+    app_addr = os.environ.get('APP_ADDR') or '127.0.0.1'
+    app_port = os.environ.get('APP_PORT') or '8000'
+    log.info('Starting Swagger validator app on {}:{}'.format(app_addr, app_port))
+    make_server(app_addr, int(app_port), create_app()).serve_forever()
