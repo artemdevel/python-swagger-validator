9020018ae35ba9462ac0225ac6651cef0a584e3e
Artem Bogomyagkov <artem.devel2014@gmail.com>
2016-01-21 17:49:35 +0200

    Changes for build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+set -x
 
-cd ..
+
+BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
+cd "$BUILD_DIR"
+
 docker build -t python_swagger_validator .
+docker run --name swagger-validator -e APP_ADDR=0.0.0.0 -dp 127.0.0.1:7777:8000 python_swagger_validator
