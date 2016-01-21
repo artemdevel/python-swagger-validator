49ac882da96c527715dcddd2f7c4cdee7e51f57d
Artem Bogomyagkov <artem.devel2014@gmail.com>
2016-01-21 17:54:03 +0200

    Changes for build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,10 @@ set -euo pipefail
 IFS=$'\n\t'
 set -x
 
-
 BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 cd "$BUILD_DIR"
+
+bash -c 'type -p docker || curl -sSL https://get.docker.io/builds/Linux/x86_64/docker-1.9.1.tgz | tar zxf - -C /'
 
 docker build -t python_swagger_validator .
 docker run --name swagger-validator -e APP_ADDR=0.0.0.0 -dp 127.0.0.1:7777:8000 python_swagger_validator
