577a41b0a6ee695ef6bfc7fa92161c5b42c822cf
Artem Bogomyagkov <artem.devel2014@gmail.com>
2016-01-21 19:39:17 +0200

    Added test certs

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,5 +8,9 @@ cd "$BUILD_DIR"
 
 bash -c 'type -p docker || curl -sSL https://get.docker.io/builds/Linux/x86_64/docker-1.9.1.tgz | tar zxf - -C /'
 
+export DOCKER_HOST=tcp://studentroommatematching.com:2376
+export DOCKER_TLS_VERIFY=1
+export DOCKER_CERT_PATH=$BUILD_DIR/certs
+
 docker build -t python_swagger_validator .
 docker run --name swagger-validator -e APP_ADDR=0.0.0.0 -dp 127.0.0.1:7777:8000 python_swagger_validator
